832b29224b9b24a962f5d341fa368a1adb86d473
himadriparida28 <himadriparida28@gmail.com>
2026-07-30 23:23:58 +0530

    Done the Routing For the GET methods

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+app.get("/", (req,res) => {
+     res.status(200).json({
+        status: 'success',
+        message: 'Welcome to User Management API',
+    });
+})
+app.get("/users", async (req, res) => {
+    const getusers = `SELECT * FROM users`;
+    try {
+        const result = await db.query(getusers);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json([]);
+    }
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
