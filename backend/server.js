7ab9c336b4d3bb808fbbe5c8fd7cf37ae015b9f3
himadriparida28 <himadriparida28@gmail.com>
2026-07-30 23:27:53 +0530

    Done the Routing for the POST Methods

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,48 @@ app.get("/users", async (req, res) => {
         res.status(500).json([]);
     }
 });
+app.post('/users', async (req, res) => {
+    const { name,registration_number , email, password, age } = req.body;
+    try {
+        const createUserQuery = `
+            INSERT INTO users ( name,registration_number , email, password,age )
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *;
+        `
+         const result = await db.query(createUserQuery, [
+            name,
+            registration_number,
+            email,
+            password,
+            age
+        ]);
+
+        res.status(201).json({
+            status: "Success",
+            message: "Created user successfully",
+            data: result.rows[0]
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "Error",
+            message: "Failed to create user",
+        });
+    }
+});
+app.post('/login',async(req, res) => {
+    const { name, password } = req.body;
+    const getUserQuery = `SELECT * FROM users WHERE name = $1 AND password = $2`;
+    try{
+        const result = await db.query(getUserQuery, [
+            name,
+            password
+        ]);
+        res.status(200).json(result.rows[0]);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to login' });
+    }
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
