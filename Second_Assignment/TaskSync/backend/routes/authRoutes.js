9ca0ec3b8a5fab72e1795b9b10a9b5c8faf4795f
himadriparida28 <himadriparida28@gmail.com>
2026-08-06 13:31:50 +0530

    Implimented the Logics and The Route For the Login,Register,getProfile and Logout

---
+++ Second_Assignment/TaskSync/backend/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const authRoute = require("express").Router();
+const {register,login,logout,getProfile} = require("../controllers/authController");
+const {authMiddleware} = require("../middlewares/authMiddleware");
+authRoute
+.post("/register", register)
+.post("/login", login)
+.post("/logout", logout)
+.get("/profile", authMiddleware, getProfile)
+module.exports = {authRoute};
